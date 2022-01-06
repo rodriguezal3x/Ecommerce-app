c8a409d3b9ac6542e57142d78b74887bc70973dc
Rodriguez Alexander <alexjosuerm1995@gmail.com>
2022-01-06 11:12:39 -0600

    Announcement

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+import React from 'react'
+
+const Container = styled.div`
+    height: 30px;
+    background-color: teal;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 14px;
+    font-weight: 500;
+`
+
+const Announcement = () => {
+    return (
+        <Container>
+            Oferta Especial Envio Gratis!!!
+        </Container>
+    )
+}
+
+export default Announcement
