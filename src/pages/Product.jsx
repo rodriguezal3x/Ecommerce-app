236075245eced5013d7735fc1d7182d85a9632c2
Rodriguez Alexander <alexjosuerm1995@gmail.com>
2022-03-02 22:58:17 -0600

    Products/Pages

---
+++ src/pages/Product.jsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import styled from 'styled-components';
+import Navbar from '../components/Navbar';
+import Announcement  from '../components/Announcement';
+import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+    padding: 50px;
+    display: flex; 
+`;
+
+const ImgContainer = styled.div`
+    flex: 1;
+`;
+
+const Image = styled.img`
+    width: 100%;
+    height: 90vh;
+    object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 0px 50px;
+`;
+
+const Title = styled.h1`
+    font-weight: 200;
+`;
+const SubTitle = styled.span`
+    margin: 20px 0px;
+`;
+const Desc = styled.p`
+    margin: 20px 0px;
+`;
+
+const Price = styled.span`
+    font-weight: 100;
+    font-size: 40px;
+`;
+// Filter Option
+const FilterContainer = styled.div`
+    width: 50%;
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Filter = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const FilterTitle = styled.span`
+    font-size: 20px;
+    font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props=>props.color};
+    margin: 0px 5px;
+    cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+    margin-left: 10px;
+    padding: 5px;
+    cursor: pointer;
+`;
+
+const FilterSizeOption = styled.option`
+`;
+
+
+const Product = () => {
+  return (
+    <Container>
+        <Announcement/>
+        <Navbar/>
+        <Wrapper>
+            <ImgContainer>
+                <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+            </ImgContainer>
+            <InfoContainer>
+                <Title>Air Nike Jordan</Title>
+                <SubTitle>Black Gold</SubTitle>
+                <Desc>Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto ficticio estándar de la industria desde el año 1500, cuando un impresor desconocido tomó una galera de tipos y la codificó para hacer un libro de muestras tipográficas. Ha sobrevivido no solo cinco siglos</Desc>
+                <Price>$ 20</Price>
+                <FilterContainer>
+                    <Filter>
+                        <FilterTitle>Color</FilterTitle>
+                        <FilterColor color="black"/>
+                        <FilterColor color="darkblue"/>
+                        <FilterColor color="gray"/>
+                    </Filter>
+                    <Filter>
+                        <FilterTitle>Size</FilterTitle>
+                        <FilterSize>
+                            <FilterSizeOption>XS</FilterSizeOption>
+                            <FilterSizeOption>S</FilterSizeOption>
+                            <FilterSizeOption>M</FilterSizeOption>
+                            <FilterSizeOption>L</FilterSizeOption>
+                            <FilterSizeOption>XL</FilterSizeOption>
+                            <FilterSizeOption>XXL</FilterSizeOption>
+                        </FilterSize>
+                    </Filter>
+                </FilterContainer>
+            </InfoContainer>
+        </Wrapper>
+        <Newsletter/>
+        <Footer/>
+    </Container>
+  );
+};
+
+export default Product;
